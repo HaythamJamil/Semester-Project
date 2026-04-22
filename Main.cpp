f79d8104e48c957c0f5c8098885c09464892bef9
HaythamJamil <muhammadhaythamjamil@gmail.com>
2026-04-22 23:16:14 +0500

    Fixed the errors in function and tested the reading ability of dirnt.h, it is working

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <dirent.h>
+#include <windows.h>
 #include <vector>
 #include <sstream>
+#include <sys/stat.h>
 using namespace std;
+vector<string> listFiles(string path)
+{
+    vector<string> files;
+    DIR *dir = opendir(path.c_str());
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL)
+    {
+        files.push_back(string(entry->d_name));
+    }
+    closedir(dir);
+    return files;
+}
+int main()
+{
+    vector<string> result = listFiles("C:\\Users\\Haytham\\Desktop");
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << endl;
+    }
+}
