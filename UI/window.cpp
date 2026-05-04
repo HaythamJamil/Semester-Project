cc989cefe41f124cb0db8e6a9b87490f7ff1b2bb
HaythamJamil <muhammadhaythamjamil@gmail.com>
2026-05-04 07:16:32 +0500

    I made it pretty, fuck you Hamza

--- UI/window.cpp
+++ UI/window.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 
-    string currentPath = "C:\\Users\\Haytham\\Desktop\\A";
+    string currentPath = "C:\\Users\\Haytham\\Desktop";
 
     vector<string> files = listFiles(currentPath);
 
@@ -20,11 +20,11 @@ int main()
     const int listStartY = 45;
 
     const int btnX = 950;    // back button position and size
-    const int delbtnX = 890; // this is for delete button
-    const int crtbtnX = 830; // this is for the create folder button
-    const int renbtnX = 770; // this is for the rename button
+    const int delbtnX = 880; // this is for delete button
+    const int crtbtnX = 810; // this is for the create folder button
+    const int renbtnX = 740; // this is for the rename button
     const int btnY = 0;
-    const int btnW = 50;
+    const int btnW = 28;
     const int btnH = 34;
 
     bool delConfirm = false;
@@ -36,6 +36,25 @@ int main()
     int selectedIndex = 0;      // first item highlighted by default
     double lastClickTime = 0.0; // time of last click
     int lastClickedIndex = -1;  // which index was last clicked
+
+    // Matrix Rain Effect variables, this is only for aesthetics
+    const int COLS = 1000 / 10; // one column every 10px
+    int rainY[COLS];            // current Y of each column head
+    int rainSpeed[COLS];        // speed of each column
+    // loop for the same effect to get random values
+    for (int i = 0; i < COLS; i++)
+    {
+        rainY[i] = GetRandomValue(-600, 0);
+        rainSpeed[i] = GetRandomValue(2, 8);
+    }
+
+    // Font Loading
+    Font matrixFont = LoadFont("MatrixCodeNFI.ttf");
+    Font uiFont = LoadFont("VT323-Regular.ttf");
+
+    // Icons Loading
+    Texture2D folderIcon = LoadTexture("folder.png");
+    Texture2D fileIcon = LoadTexture("paper.png");
 
     while (!WindowShouldClose())
     {
@@ -89,6 +108,17 @@ int main()
                 files = listFiles(currentPath);
                 newName = "";
                 renConfirm = false;
+            }
+        }
+
+        // matrix rain update
+        for (int i = 0; i < COLS; i++)
+        {
+            rainY[i] += rainSpeed[i];
+            if (rainY[i] > 600)
+            {
+                rainY[i] = GetRandomValue(-300, 0);
+                rainSpeed[i] = GetRandomValue(2, 8);
             }
         }
         // mouse click
@@ -179,19 +209,38 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
+        // matrix rain draw
+        for (int i = 0; i < COLS; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                int x = i * 10;
+                int y = rainY[i] - j * 15;
+                int alpha = 255 - (j * 15);
+                if (alpha < 0)
+                    alpha = 0;
+                Color rainColor = {0, (unsigned char)(180 - j * 8), 0, (unsigned char)alpha};
+                char ch[2] = {(char)GetRandomValue(33, 126), '\0'};
+                DrawTextEx(matrixFont, ch, {(float)x, (float)y}, 14, 1, rainColor);
+            }
+        }
+
+        // this makes the rain effect damper
+        DrawRectangle(0, 35, 770, 600, {0, 0, 0, 160});
+
         // current path
         DrawText(currentPath.c_str(), 10, 10, 16, RED);
-        DrawLine(0, 35, 500, 35, LIGHTGRAY);
+        DrawLine(0, 35, renbtnX - 2, 35, LIGHTGRAY);
 
         // back button
-        DrawRectangle(btnX, btnY, btnW, btnH, ORANGE);
-        DrawText("<-", btnX + 10, btnY + 5, 32, WHITE);
-        DrawRectangle(delbtnX, btnY, btnW + 5, btnH, RED);
-        DrawText("DEL", delbtnX + 2, btnY + 5, 28, WHITE);
-        DrawRectangle(crtbtnX, btnY, btnW + 5, btnH, ORANGE);
-        DrawText("CRT", crtbtnX + 3, btnY + 5, 28, WHITE);
-        DrawRectangle(renbtnX, btnY, btnW + 5, btnH, LIME);
-        DrawText("REN", renbtnX + 3, btnY + 5, 28, WHITE);
+        DrawRectangle(btnX, btnY, btnW + 5, btnH, BLUE);
+        DrawTextEx(uiFont, "<-", {(float)btnX + 5, (float)btnY + 5}, 32, 1, WHITE);
+        DrawRectangle(delbtnX, btnY, btnW + 9, btnH, RED);
+        DrawTextEx(uiFont, "DEL", {(float)delbtnX + 2, (float)btnY + 5}, 28, 1, WHITE);
+        DrawRectangle(crtbtnX, btnY, btnW + 10, btnH, ORANGE);
+        DrawTextEx(uiFont, "CRT", {(float)crtbtnX + 2, (float)btnY + 5}, 28, 1, WHITE);
+        DrawRectangle(renbtnX, btnY, btnW + 9, btnH, LIME);
+        DrawTextEx(uiFont, "REN", {(float)renbtnX + 2, (float)btnY + 5}, 28, 1, WHITE);
 
         // ONE loop only
         for (int i = 0; i < (int)files.size(); i++)
@@ -203,39 +252,51 @@ int main()
             if (i == selectedIndex)
             {
                 DrawRectangleLines(10, y - 2, 980, rowHeight, ORANGE);
-                DrawText(files[i].c_str(), 20, y, 18, ORANGE);
+                if (isFolder(currentPath, files[i]))
+                    DrawTextureEx(folderIcon, {2, (float)y}, 0, 0.035f, ORANGE);
+                else
+                    DrawTextureEx(fileIcon, {4, (float)y}, 0, 0.028f, ORANGE);
+                DrawTextEx(uiFont, files[i].c_str(), {(float)20, (float)y}, 24, 1, ORANGE);
                 string size = sizeSorter(fileSize(currentPath, files[i]));
-                DrawText(size.c_str(), 900, y, 18, RED);
+                DrawTextEx(uiFont, size.c_str(), {(float)900, (float)y}, 24, 1, RED);
                 string type = fileType(files[i]);
-                DrawText(type.c_str(), 775, y, 18, YELLOW);
+                DrawTextEx(uiFont, type.c_str(), {(float)775, (float)y}, 24, 1, YELLOW);
             }
             else
             {
-                DrawText(files[i].c_str(), 20, y, 18, GREEN);
+                if (isFolder(currentPath, files[i]))
+                    DrawTextureEx(folderIcon, {2, (float)y}, 0, 0.035f, GREEN);
+                else
+                    DrawTextureEx(fileIcon, {4, (float)y}, 0, 0.028f, GREEN);
+                DrawTextEx(uiFont, files[i].c_str(), {(float)20, (float)y}, 22, 1, GREEN);
             }
         }
 
         if (delConfirm)
         {
             DrawRectangle(250, 200, 400, 150, DARKGRAY);
-            DrawText("Are you sure you want to delete?", 270, 230, 18, WHITE);
+            DrawTextEx(uiFont, "Are you sure you want to delete?", {(float)270, (float)230}, 25, 1, WHITE);
             DrawText("YES", 320, 310, 20, RED);
             DrawText("NO", 520, 310, 20, GREEN);
         }
         if (crtConfirm)
         {
             DrawRectangle(250, 200, 400, 150, DARKGRAY);
-            DrawText("Name your New Folder:", 270, 230, 18, WHITE);
-            DrawText(newName.c_str(), 270, 270, 18, GREEN);
+            DrawTextEx(uiFont, "Name your New Folder:", {(float)270, (float)230}, 25, 1, WHITE);
+            DrawTextEx(uiFont, newName.c_str(), {(float)270, (float)270}, 22, 1, GREEN);
             DrawText("Press ENTER to confirm", 270, 310, 16, GRAY);
         }
         if (renConfirm)
         {
             DrawRectangle(250, 200, 400, 150, DARKGRAY);
-            DrawText("Rename your Item:", 270, 230, 18, WHITE);
-            DrawText(newName.c_str(), 270, 270, 18, GREEN);
+            DrawTextEx(uiFont, "Rename your Item:", {(float)270, (float)230}, 25, 1, WHITE);
+            DrawTextEx(uiFont, newName.c_str(), {(float)270, (float)270}, 22, 1, GREEN);
             DrawText("Press ENTER to confirm", 270, 310, 16, GRAY);
         }
+        UnloadTexture(folderIcon);
+        UnloadTexture(fileIcon);
+        UnloadFont(uiFont);
+        UnloadFont(matrixFont);
 
         EndDrawing();
     }
